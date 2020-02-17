27a6434552afaf51cf63214c5a51a717501cfd2c
Michael Harms <michael.harms6010@gmail.com>
2020-02-17 15:38:05 -0600

    xd return oopsie

--- data/migrations/20200217145325_adds-description-column.js
+++ data/migrations/20200217145325_adds-description-column.js
@@ -1,13 +1,13 @@
 
 exports.up = function(knex) {
-    knex.schema.table('users', function (table) {
+    return knex.schema.table('users', function (table) {
         table.string("description", 512);
         table.string("viewkey", 256);
       })
 };
 
 exports.down = function(knex) {
-    knex.schema.table('users', function(table){
+    return knex.schema.table('users', function(table){
         table.dropColumn('description');
         table.dropColumn("viewkey");
       })
