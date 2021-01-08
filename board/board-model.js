b28cd2fd36a83794791b00b52526440cdfa7407f
Michael Harms <michael.harms6010@gmail.com>
2021-01-08 09:29:07 -0600

    adds reply zaddr column and grabber

--- board/board-model.js
+++ board/board-model.js
@@ -1,5 +1,6 @@
 const db = require("../data/db-config.js");
 const likeRegex = /LIKE::(\d+)/i
+const zaddrRegex = /^zs[a-z0-9]{76}$/i;
 
 module.exports = {
     getAll,
@@ -36,10 +37,15 @@ function getPage(page) {
 async function add(post) {
     if (post.memo.match(likeRegex)) {
         const like = post.memo.match(likeRegex)[0]
-        const postId = like.split("::")[1]
+        const postId = like.split("::")[1].split(" ")[0]
         const likedPost = await db('board_posts').where({id: postId}).first()
         return db('board_posts').where({id: postId}).update({likes: likedPost.likes + 1})
     } else {
+        if (post.memo.match(zaddrRegex)) {
+            const replyZaddr = post.memo.match(zaddrRegex)[0];
+            post.reply_zaddr = replyZaddr;
+        }
+
         return db('board_posts').insert(post).returning("*")
     }
 }
