97a5f31c8692ac844a0c778601a43a00556c8889
Michael Harms <michael.harms6010@gmail.com>
2021-01-13 22:15:59 -0600

    adds ispoll boolean

--- data/migrations/20210109204622_add_ispoll_to_post.js
+++ data/migrations/20210109204622_add_ispoll_to_post.js
@@ -1,12 +1,12 @@
 
 exports.up = function(knex) {
     return knex.schema.table('board_posts', function (table) {
-        table.integer("reply_count").defaultTo(0);
+        table.boolean("ispoll").defaultTo(false);
       })
 };
 
 exports.down = function(knex) {
     return knex.schema.table('board_posts', function(table){
-        table.dropColumn('reply_count');
+        table.dropColumn('ispoll');
       })
 };
