ff0b951995a2924873f81bf8b57c107582fdbec0
Michael Harms <michael.harms6010@gmail.com>
2021-01-24 22:13:51 -0600

    adds rate limiter

--- index.js
+++ index.js
@@ -3,7 +3,17 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+const rateLimit = require("express-rate-limit");
 
+const dataLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000, // 15 minutes
+    max: 500
+  });
+
+  const authLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 25
+  });
 
 
 const usersRouter = require("./users/users-router.js")
@@ -17,9 +27,9 @@ server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"));
 
-server.use("/users", usersRouter);
-server.use("/auth", authRouter);
-server.use("/board", boardRouter);
+server.use("/users", dataLimiter, usersRouter);
+server.use("/auth", authLimiter, authRouter);
+server.use("/board", dataLimiter, boardRouter);
 
 
 
