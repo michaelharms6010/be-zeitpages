68df7ae9c9060db144ffc180eb6613896f685abe
Michael Harms <michael.harms6010@gmail.com>
2021-01-27 11:14:44 -0600

    typo

--- users/users-router.js
+++ users/users-router.js
@@ -36,7 +36,7 @@ router.get("/me", restricted, (req,res) => {
 })
 
 router.get("/getusernames", restricted, (req,res) => {
-    Users.getUseranmes().then(user => {
+    Users.getUsernames().then(user => {
         delete user.password;
         res.status(201).json({usernames: user.map(u => u.username)})
     })
