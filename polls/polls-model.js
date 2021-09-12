8159b7971e0a3ccc766d20a13b04e474aa6e3b7c
Michael Harms <michael.harms6010@gmail.com>
2021-09-11 21:40:35 -0500

    erm?

---
+++ polls/polls-model.js
@@ -0,0 +1,36 @@
+const db = require("../data/db-config")
+
+
+module.exports = {
+    getPollVotes,
+    getPollResults
+}
+
+function getPollVotes(poll_id) {
+    return db("votes").where({poll_id})
+}
+
+async function getPollResults(poll_id) {
+    try {
+
+        const votes = await getPollVotes(poll_id)
+        const pollEntry = await db("board_posts").where({id: poll_id}).first()
+        console.log("PollEntry", pollEntry)
+        const poll = JSON.parse(pollEntry.memo.replace("POLL::", ""))
+        console.log("poll", poll)
+        const results = {}
+        results.q = poll.q
+        for (let i = 1; i < 5; i++)
+
+            if (poll[`o${i}`]) {
+                results[poll[`o${i}`]] = votes.filter(v => v.option == i).length
+            }
+        
+        
+        return results
+    } catch (e) {
+        console.log(e)
+        return
+    }
+
+}
